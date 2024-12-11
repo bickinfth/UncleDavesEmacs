ed483561f880289a3e8c79ae26d5e7d1a1db5399
bickinfth <nesetfatih.bickin@std.yeditepe.edu.tr>
2024-12-11 15:26:10 +0300

    Inıt.el içerisinde outdate olan packagelar için düzeltmeler yapıldı

--- init.el
+++ init.el
@@ -10,41 +10,54 @@
 
 (defun startup/reset-gc ()
   (setq gc-cons-threshold 16777216
-	gc-cons-percentage 0.1))
+        gc-cons-percentage 0.1))
 
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; This is all kinds of necessary
+;;; Package management
 (require 'package)
 (setq package-enable-at-startup nil)
-
-;;; remove SC if you are not using sunrise commander and org if you like outdated packages
-(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
-			 ("gnu"   . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("org"   . "https://orgmode.org/elpa/")))
+(setq package-archives '(("ELPA"  . "https://tromey.com/elpa/")
+                         ("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; Bootstrapping use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'use-package)
 
-;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+;;; Jedi configuration for Python
+(use-package jedi
+  :ensure t
+  :config
+  (setq jedi:complete-on-dot t) ;; Enable autocompletion after "."
+  (setq jedi:environment-root "default") ;; Name of the virtual environment
+  (setq jedi:environment-virtualenv
+        '("python3" "-m" "venv")) ;; Command to create the virtual environment
+  (add-hook 'python-mode-hook 'jedi:setup)) ;; Add Jedi to Python mode
+
+;;; Optional: Automatically install Jedi server if not present
+(defun ensure-jedi-server ()
+  "Ensure the Jedi server is installed and configured."
+  (interactive)
+  (unless (file-exists-p "~/.emacs.d/.python-environments/default")
+    (jedi:install-server)))
+
+(add-hook 'python-mode-hook 'ensure-jedi-server)
+
+;;; Load config.org if it exists
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
-;;; Experimental email stuff.
+;;; Email configuration if exists
 (when (file-readable-p "~/.email/email.org")
   (org-babel-load-file (expand-file-name "~/.email/email.org")))
 
-;;; Anything below is personal preference.
-;;; I recommend changing these values with the "customize" menu
-;;; You can change the font to suit your liking, it won't break anything.
-;;; The one currently set up is called Terminus.
+;;; Customization
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,14 +66,15 @@
  '(ansi-color-names-vector
    ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(package-selected-packages
-   '(maxima f auctex telephone-line love-minor-mode auto-complete slime-company slime company-jedi zzz-to-char rainbow-delimiters avy ivy projectile sunrise-x-modeline sunrise-x-buttons sunrise-commander twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package))
+   '(jedi maxiuma f auctex telephone-line auto-complete rainbow-delimiters ivy projectile 
+          magit org-bullets use-package))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#abb2bf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 138 :width normal :foundry "UKWN" :family "Nimbus Mono PS"))))
- '(fringe ((t (:background "#292b2e")))))
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "1ASC" :family "Monospace"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" 
+               :foreground "#abb2bf" :inverse-video nil :box nil 
+               :strike-through nil :overline nil :underline nil 
+               :slant normal :weight regular :height 138 :width normal 
+               :foundry "UKWN" :family "Nimbus Mono PS")))))
+
